981556cd85b1cfdd73d3f75403fff17e0238a44b
RadeshShiwlochan <radesh0430@gmail.com>
2016-07-07 17:35:17 -0400

    checks for duplicate users and reports an error

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.url);
+require('./config/passport')(passport);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -18,15 +21,18 @@ app.use(session({secret: 'anystringoftext',
 	            resave: true}));
 
 app.use(bodyParser.urlencoded({extended: false}));
-require('./app/routes.js')(app);
+
 // app.use('/', function(req, res) {
 // 	res.send("Our first express program!");
 // 	console.log(req.cookies);
 // 	console.log('=============');
 // 	console.log(req.session);
 // });
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 app.set('view engine', 'ejs');
+require('./app/routes.js')(app,passport);
 
 app.listen(port);
 console.log("app running on 8080");
