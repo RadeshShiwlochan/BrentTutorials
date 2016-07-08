20b6f93574170cb97e408e5b4a243eb869970439
RadeshShiwlochan <radesh0430@gmail.com>
2016-07-07 22:26:23 -0400

    fixes login, user can now log into their account

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,15 @@ module.exports = function(app,passport) {
 		res.render('index.ejs');
 	});
 
+	app.get('/login', function(req, res){
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
+
 	app.get('/signup', function(req, res) {
 		res.render('signup.ejs', { message: req.flash('signupMessage')});
 	});
@@ -28,6 +37,10 @@ module.exports = function(app,passport) {
 	// 	res.redirect('/');
 	// });
 
+	app.get('/profile', isLoggedIn, function(req, res){
+		res.render('profile.ejs', { user: req.user });
+	});
+
 	app.get('/:username/:password', function(req, res) {
 		var newUser = new User();
 		newUser.local.username = req.params.username;
@@ -42,4 +55,18 @@ module.exports = function(app,passport) {
 		//req.params.password
 		res.send("Success!");
 	});
+
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	});
+}
+
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+
+	res.redirect('/login');
 }
